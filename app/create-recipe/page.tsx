6df976bd9f970cae0ce1d1c78b83774ebf941594
tomiwa-code <ogunbaseayoola@gmail.com>
2025-07-11 14:59:22 +0100

    Fix: fixed build error

--- app/create-recipe/page.tsx
+++ app/create-recipe/page.tsx
@@ -2,7 +2,17 @@ import React from "react";
 import CreateRecipeWrapper from "./components/CreateRecipeWrapper";
 
 const CreateRecipePage = () => {
-  return <CreateRecipeWrapper />;
+  return (
+    <React.Suspense
+      fallback={
+        <div className="min-h-[70vh] flex items-center justify-center w-full ">
+          <p>...Loading content</p>
+        </div>
+      }
+    >
+      <CreateRecipeWrapper />
+    </React.Suspense>
+  );
 };
 
 export default CreateRecipePage;
